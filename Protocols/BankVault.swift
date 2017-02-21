7cbc5c5f40cab8af9274ce0b44b6b61565be5dc8
bgb0923 <bgb0923@gmail.com>
2017-02-21 14:19:48 -0500

    Completed Lab

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -7,7 +7,7 @@
 //
 
 
-class BankVault {
+class BankVault: ProvideAccess {
     
     let name: String
     let address: String
@@ -20,7 +20,28 @@ class BankVault {
     
 }
 
+protocol ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool
+}
 
 
+extension BankVault {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        var isValid : Bool
+        if password.isEmpty {
+            return false
+        } else if password.count > 10 {
+            return false
 
-
+        } else {
+            for number in 0..<password.count {
+                if number % 2 == 0 {
+                    isValid = password[number] % 2 == 0
+                    if !isValid { return false }
+                }
+            }
+            return true
+        
+        }
+    }
+}
